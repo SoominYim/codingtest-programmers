ef51556814967c245cf582fd475fc60688fc08cc
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-14 17:33:42 +0900

    [level unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -1,6 +1,6 @@
 const solution = (ineq, eq, n, m) =>
-(ineq === "<" && eq === "=" && n <= m) ||
++((ineq === "<" && eq === "=" && n <= m) ||
 (ineq === ">" && eq === "=" && n >= m) ||
 (ineq === "<" && eq === "!" && n < m) ||
 (ineq === ">" && eq === "!" && n > m)
-? 1 : 0;
+);
