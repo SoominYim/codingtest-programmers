41c4e2de2c41148e6f52f9844b54190221a96e0e
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-14 17:46:15 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 3.62 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,5 @@
+const solution = (arr, queries) =>
+  queries.map(([s, e, k]) => {
+    const filtered = arr.slice(s, e + 1).filter(num => num > k);
+    return filtered.length > 0 ? Math.min(...filtered) : -1;
+  });
