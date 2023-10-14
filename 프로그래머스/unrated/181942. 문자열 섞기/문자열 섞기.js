c2cf8274ab195e6467a9fc0eb626c0b0149bdb42
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-14 20:09:26 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,7 +1,1 @@
-function solution(str1, str2) {
-    var result = "";
-    for (let i = 0; i < str1.length ; i++){
-        result += str1[i] + str2[i]
-    }
-    return result
-}
+const solution = (str1, str2) => [...str1].map((x, idx)=> x+str2[idx]).join("");
