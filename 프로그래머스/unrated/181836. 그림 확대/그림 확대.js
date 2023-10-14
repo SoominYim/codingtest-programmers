00b7b7d0be9e1250d368e6859e661b41a276243f
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-14 17:06:34 +0900

    [level unrated] Title: 그림 확대, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -1,5 +1,5 @@
 function solution(p, k) {
-  return p.reduce((acc, cur) => {
+    return p.reduce((acc, cur) => {
     const r = cur.split('').map(v => v.repeat(k)).join('');
     for(let i = 0; i < k; i++){
       acc.push(r)
