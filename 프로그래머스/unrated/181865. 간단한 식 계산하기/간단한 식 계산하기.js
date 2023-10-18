eae73e5c8db4ae4621ea72dfee27a35403a62302
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-18 13:55:38 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,1 +1,4 @@
-const solution = binomial=> eval(binomial)
+const solution = binomial => {
+  const b = binomial.split(' ')
+  return b[1] === "+" ? +b[0] + +b[2] : b[1] === "-" ? +b[0] - +b[2] : +b[0] * +b[2]
+}
