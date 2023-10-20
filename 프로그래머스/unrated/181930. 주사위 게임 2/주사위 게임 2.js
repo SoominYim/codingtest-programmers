cb8b3dbf3cd1fc9b33fc35f33be3f06ab6fbc2f5
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-20 17:06:43 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,2 @@
+const solution = (a,b,c) => a === b && b === c ? (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c) : a === b || b === c || a === c ? (a + b + c) * (a * a + b * b + c * c) : a + b + c;
+
