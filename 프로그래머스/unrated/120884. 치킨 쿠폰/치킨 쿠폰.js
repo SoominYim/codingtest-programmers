6013fd9020a1566c0dc75c70a10209fbe3db8976
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-10-24 11:41:28 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,9 @@
+const solution = chicken => {
+  let answer = 0;
+
+  while (chicken >= 10) {
+    answer +=  Math.floor(chicken / 10);
+    chicken = chicken -  Math.floor(chicken / 10) * 10 +  Math.floor(chicken / 10);
+  }
+  return answer;
+};
