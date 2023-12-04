cbf44f0d0be1a9d9a014eb8ce7479c2f0713bcb7
수빡이 <107674737+SoominYim@users.noreply.github.com>
2023-12-04 16:10:15 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,10 @@
+const solution = (n,words) => {
+  for ( let i = 1; i < words.length; i++ ) {
+    if (
+      words[i-1].slice(-1) !== words[i][0] ||
+      words.slice(0,i).includes(words[i])
+    )
+      return [i % n + 1, parseInt(i / n) + 1]
+  }
+  return [0, 0]
+}
