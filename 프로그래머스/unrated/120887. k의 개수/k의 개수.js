13cd1642471b751af07ee9aa5644dd4f27b3f637
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-01-10 15:56:40 +0900

    [level unrated] Title: k의 개수, Time: 5.20 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.js
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.js
@@ -1,4 +1,2 @@
-const solution = (i,j,k) => {
-  const arr = Array.from({length : j-i+1},(_,idx)=> i+idx).join('')
-  return arr.split(k).length-1
-}
+const solution = (i,j,k) =>  Array.from({length : j-i+1},(_,idx)=> i+idx).join('').split(k).length-1
+
