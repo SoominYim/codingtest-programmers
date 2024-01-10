5f3b952be246dcacce2335d1ef85c7fcff2490fe
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-01-10 17:41:47 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
@@ -1,11 +1,1 @@
-const solution = (A, B) => {
-  let arr = [...A];
-  if(A===B) return 0
-  for(let i=1; i<arr.length; i++){
-    arr.unshift(arr.pop())
-    if(arr.join('') === B){
-      return i
-    }
-  }
-  return -1
-};
+const solution = (A, B) => B.repeat(2).indexOf(A)
