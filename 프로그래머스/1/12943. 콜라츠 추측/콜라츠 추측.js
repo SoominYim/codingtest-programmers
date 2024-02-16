cba0b6ce9176a3a93305edf8bc0702b6e96c2569
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-02-16 17:15:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,9 +1,1 @@
-const solution = num => {
-  let count = 0;
-  while (num !== 1) {
-    num = num % 2 === 0 ? num / 2 : num * 3 + 1;
-    count++;
-    count > 500 ? (num = 1) : null;
-  }
-  return count > 500 ? -1 : count;
-};
+const solution=num=>(c=n=>n==1?0:n%2==0?c(n/2)+1:c(n*3+1)+1)(num)>=500?-1:c(num);
