ca53127928a54c03a975ccdf8fefe1381a28adb4
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-02-26 17:28:15 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,8 +1,1 @@
-const solution = (left, right) => Array.from({ length: (right - left) + 1 }, (_, i) => {
-        const num = left + i;
-        const divisorCount = Array.from({ length: num }, (_, j) => j + 1)
-            .filter((v) => num % v === 0)
-            .length;
-        return divisorCount % 2 === 0 ? num : -num;
-    }).reduce((acc, cur) => acc + cur, 0);
-  
+const solution=(left,right)=>Array.from({length:right-left+1},(_,i)=>i+left).reduce((acc,cur)=>acc+(Number.isInteger(Math.sqrt(cur))?-1:1)*cur,0);
