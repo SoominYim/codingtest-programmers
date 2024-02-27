982cf3d884b3072f6f097b7b7f5432d7bc8c5a3c
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-02-27 16:30:29 +0900

    [level 2] Title: 올바른 괄호, Time: 20.26 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,9 +1,1 @@
-const solution = s => {
-  let stack = 0;
-  
-  for(let i = 0; i < s.length; i++){
-    stack += s[i] === "(" ? 1 : -1
-    if(stack < 0) return false
-  }
-  return stack === 0 ? true : false
-}
+const solution=s=>![...s].reduce((a,c)=>(a<0?a:(c=='('?++a:--a)),0)
