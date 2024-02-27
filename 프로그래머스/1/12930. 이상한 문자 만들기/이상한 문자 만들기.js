ff42c9b19a6c774672f82166636527f41cac6020
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-02-27 18:29:42 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,1 +1,1 @@
-const solution = s => s.split(' ').map(v=>[...v].map((x,i) => i%2===0 ? x.toUpperCase() : x.toLowerCase()).join('')).join(' ')
+const solution=s=>s.split(' ').map(v=>[...v].map((v,i)=>i%2==0?v.toUpperCase():v.toLowerCase()).join('')).join(' ')
