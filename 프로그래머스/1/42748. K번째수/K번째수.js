9c8e3332cc03997757ee55d61258abb395c99cd6
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-02-28 11:13:01 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,1 +1,1 @@
-const solution = (array,commands) => commands.map(v=>array.slice(v[0]-1,v[1]).sort((a,b) => a-b)[v[2]-1])
+const solution=(array,commands)=>commands.reduce((a,[i,j,k])=>[...a,array.slice(i-1,j).sort((a,b)=>a-b)[k-1]],[])
