49d3410af744e263252b593abce24cf285df9405
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-07-25 10:59:18 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,1 +1,11 @@
-const solution = p => p >= 500000 ? Math.floor(p * 0.8) : p >= 300000 ? Math.floor(p * 0.9) : p >= 100000 ? Math.floor(p * 0.95) : p;
+function solution(price) {
+    ;
+   if (price >= 500000){
+        price = parseInt(price-(price*0.2));
+    }else if (price >= 300000 && price < 500000){
+        price = parseInt(price-(price*0.1));
+    }else if (price >= 100000 && price < 300000){
+        price = parseInt(price-(price*0.05));
+    }
+    return price;
+}
