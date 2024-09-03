e62c4c83bb9ac771f6279148cf517c140b5b7c81
수빡이 <107674737+SoominYim@users.noreply.github.com>
2024-09-03 16:35:54 +0900

    [level 2] Title: 카펫, Time: 8.75 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -3,7 +3,10 @@ const solution = (brown,yellow) => {
   let TOTAL = brown+yellow
  
   for(let i = 2; i < TOTAL/2; i++){
-      if(TOTAL%i===0) a.push([TOTAL/i,i]);
+      if(TOTAL%i===0) {
+          a.push([TOTAL/i,i])
+          
+      };
   }
   return a.filter(v=>(v[0]+v[1])*2-4 === brown)[0];
 }
