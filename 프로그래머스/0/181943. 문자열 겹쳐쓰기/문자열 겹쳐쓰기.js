a7edecaa71efcda53a0a98766a958e1c674f60f8
수빡이 <107674737+SoominYim@users.noreply.github.com>
2026-03-26 17:04:41 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,1 +1,3 @@
-const solution = (m, o, s) => m.slice(0,s)+o+m.slice(o.length+s,m.length)
+function solution(my_string, overwrite_string, s) {
+    return [my_string.slice(0,s),overwrite_string,my_string.slice(s+overwrite_string.length)].join('');
+}
